56ac3d39f56f2555e6cdde88d953314f2c1a687a
Sevgi1981 <108486554+Sevgi1981@users.noreply.github.com>
2022-07-07 21:10:34 -0400

    Update SevgiNewClass.java

--- SevginewTest/SevgiNewClass.java
+++ SevginewTest/SevgiNewClass.java
@@ -3,5 +3,8 @@ package SevginewTest;
 public class SevgiNewClass {
     public static void main(String[] args) {
         System.out.println("this is sevgiNew test");
+        System.out.println("this is sevgiNew test222");
+         System.out.println("this is sevgiNew test333");
+   
     }
 }
